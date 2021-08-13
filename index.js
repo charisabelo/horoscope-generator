1168ae409cefcbc6a24d4ba0dd48906dff107588
Char Isabelo <charisseisabelo@gmail.com>
2021-08-13 01:24:18 -1000

    started js functionality

---
+++ index.js
@@ -0,0 +1,36 @@
+const aries = document.querySelector(".main__aries");
+const taurus = document.querySelector(".main__taurus");
+const gemini = document.querySelector(".main__gemini");
+const cancer = document.querySelector(".main__cancer");
+const leo = document.querySelector(".main__leo");
+const virgo = document.querySelector(".main__virgo");
+const libra = document.querySelector(".main__libra");
+const scorpio = document.querySelector(".main__scorpio");
+const sagittarius = document.querySelector(".main__sagittarius");
+const capricorn = document.querySelector(".main__capricorn");
+const aquarius = document.querySelector(".main__aquarius");
+const pisces = document.querySelector(".main__pisces");
+
+aries.addEventListener("click", function () {});
+
+taurus.addEventListener("click", function () {});
+
+gemini.addEventListener("click", function () {});
+
+aries.addEventListener("click", function () {});
+
+aries.addEventListener("click", function () {});
+
+aries.addEventListener("click", function () {});
+
+aries.addEventListener("click", function () {});
+
+aries.addEventListener("click", function () {});
+
+aries.addEventListener("click", function () {});
+
+aries.addEventListener("click", function () {});
+
+aries.addEventListener("click", function () {});
+
+aries.addEventListener("click", function () {});
